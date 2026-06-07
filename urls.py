08038af269d6382d2a2b3de4f9fe7d6ced025009
Roman Gaidukov <roman72gqaz@yandex.ru>
2026-06-07 11:58:57 +0300

    Вынес адрес сервера в отдельную переменную.

--- urls.py
+++ urls.py
@@ -1,4 +1,4 @@
 MAIN_URL = "https://stellarburgers.education-services.ru" # Главная страница.
-REGISTER_URL = "https://stellarburgers.education-services.ru/register" # Страница формы регистрации.
-LOGIN_URL = "https://stellarburgers.education-services.ru/login" # Страница формы входа в аккаунт.
-FORGOT_URL = "https://stellarburgers.education-services.ru/forgot-password" # Страница формы востановления пароля.
+REGISTER_URL = f"{MAIN_URL}/register" # Страница формы регистрации.
+LOGIN_URL = f"{MAIN_URL}/login" # Страница формы входа в аккаунт.
+FORGOT_URL = f"{MAIN_URL}/forgot-password" # Страница формы востановления пароля.
